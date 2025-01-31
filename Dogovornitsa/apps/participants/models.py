8858caa784d3e52a27253750216161b1be13c4d8
batoncss <batoncss@yandex.ru>
2025-01-31 15:57:55 +0300

    a little homework

--- Dogovornitsa/apps/participants/models.py
+++ Dogovornitsa/apps/participants/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Participant(models.Model):
-    name = models.CharField('Наименование клиента', max_length=50, null=True, default=None)
-    legal_address = models.CharField('Юридический адрес', max_length=50, null=True, default=None)
-    actual_address = models.CharField('Фактический адрес', max_length=50, null=True, default=None)
-    inn = models.CharField('ИНН', null=True, default="", max_length=12, unique=True)
-    kpp = models.CharField('КПП', null=True, default=None, max_length=9)
-    ogrn = models.CharField('ОГРН', null=True, default=None, max_length=15)
+    name = models.CharField('Наименование клиента', max_length=50)
+    legal_address = models.CharField('Юридический адрес', max_length=50)
+    actual_address = models.CharField('Фактический адрес', max_length=50)
+    inn = models.CharField('ИНН', max_length=12)
+    kpp = models.CharField('КПП', max_length=9)
+    ogrn = models.CharField('ОГРН', max_length=13)
 
     def __str__(self):
         return f"{self.name} (ИНН: {self.inn})"
--- Dogovornitsa/urls.py
+++ Dogovornitsa/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from Dogovornitsa.views import templates_home
 
 urlpatterns = [
+    path('', templates_home, name='home'),
     path('admin/', admin.site.urls),
     path('participants/', include('participants.urls')),
     path('templates/', include('templates.urls')),
     path('orders/', include('orders.urls')),
-    path('', include('pages.urls')),
 ]
